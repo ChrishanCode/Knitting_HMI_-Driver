bebbf9bb4bd923c03974ffb7208a1ade25b70227
DESKTOP-QF7OOTB\LENOVO <chrishan.utech@gmail.com>
2025-06-03 14:29:25 +0530

    remove the color change on breakdwon complete on 03.06.2025

--- src/nextion_to_ESP.cpp
+++ src/nextion_to_ESP.cpp
@@ -510,22 +510,22 @@ void pageController()
 
             if (deviceId == "M01")
             {
-                statusID_01 = 3;
+                // statusID_01 = 3;
                 errorCode_M01 = "0";
             }
             else if (deviceId == "M02")
             {
-                statusID_02 = 3;
+                // statusID_02 = 3;
                 errorCode_M02 = "0";
             }
             else if (deviceId == "M07")
             {
-                statusID_07 = 3;
+                // statusID_07 = 3;
                 errorCode_M07 = "0";
             }
             else if (deviceId == "M23")
             {
-                statusID_23 = 3;
+                // statusID_23 = 3;
                 errorCode_M23 = "0";
             }
             else
@@ -624,8 +624,4 @@ void pageController()
     default:
         break;
     }
-}
-
-void fristTimeSetup()
-{
 }
